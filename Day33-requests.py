f9e295faa88954df4b8e25daed6846cfb9e11ae6
Ling9150 <3543306507@qq.com>
2026-04-10 16:09:39 +0800

    python的学习记录

--- Day33-requests.py
+++ Day33-requests.py
@@ -2,6 +2,7 @@ import requests
 import json
 package = requests.get("https://api.ipify.org?format=json")
 print(package.status_code)
+#改进做法：if package.status_code == 200
 json_data = package.text
-data = json.loads(json_data)
-print(f"获取到{data}")
+data = json.loads(json_data) #解析功能已有，可以直接这样写：data = package.json()
+print(f"获取到{data}") #改进做法：data["ip"]

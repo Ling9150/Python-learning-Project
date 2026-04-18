7e81c1aa4f9e6dc9d81560f89b7728cfca2b274b
Ling9150 <3543306507@qq.com>
2026-04-18 22:49:26 +0800

    python的学习记录

---
+++ Day41-os.py
@@ -0,0 +1,15 @@
+import os
+print(f"我现在的位置是{os.getcwd()}") #我在哪？
+files = os.listdir('.')
+print(files)
+os.makedirs("test_dir",exist_ok=True)
+base_dir = "Network_Logs"
+today = "2026-04-18"
+target_path = f"{base_dir}/{today}"
+r = os.path.exists(target_path)
+if r == "True":
+    print("已存在")
+else:
+    os.makedirs(target_path)
+    print("文件夹已就绪")
+

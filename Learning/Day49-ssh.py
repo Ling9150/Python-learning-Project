cffe40e84b80367907c2db27e746ab33dbc2357e
Ling9150 <3543306507@qq.com>
2026-07-13 23:14:02 +0800

    python learning

--- Learning/Day49-ssh.py
+++ Learning/Day49-ssh.py
@@ -14,9 +14,7 @@ try:
     result = stdout.read().decode('utf-8')
     print(f"文件:{result}")
 except Exception as e:
-    res = stderr.read().decode('utf-8')
-    print(res)
-    print(f"失败:{stderr}")
+    print(f"失败:{e}")
 finally:
     ssh.close()
     print("结束")

65ae59ab12da018fce093b9d4704c651843b1b85
Ling9150 <3543306507@qq.com>
2026-07-17 10:26:00 +0800

    python learning

--- Learning/Day33-requests.py
+++ Learning/Day33-requests.py
@@ -6,8 +6,13 @@ print(package.status_code)
 json_data = package.text
 data = json.loads(json_data) #解析功能已有，可以直接这样写：data = package.json()
 print(f"获取到{data}") #改进做法：data["ip"]
-res = requests.get("https://books.toscrape.com/")
+'''res = requests.get("https://books.toscrape.com/")
 if res.ok:
     print(res.text)
 else:
-    print("失败")
+    print("失败")'''
+head = {
+    "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/150.0.0.0 Safari/537.36"
+}
+r = requests.get("https://movie.douban.com/top250",headers=head)
+print(r.status_code)
